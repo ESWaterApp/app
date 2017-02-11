5360e76bb6c064ee32ea068d1cae1098745daf7d
PGrad <paulgrad3rd@gmail.com>
2017-02-11 02:04:31 -0800

    Removed unnecessary parameter

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('waterUCICtrl', ['$scope', '$stateParams', '$ionicSideMenuDelegate', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('waterUCICtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
